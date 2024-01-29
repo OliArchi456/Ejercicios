39244dc9c0cb0fdd18fb73998e57eb0d6fd00c10
OliArchi456 <158003676+OliArchi456@users.noreply.github.com>
2024-01-28 20:19:49 -0500

    Update ejercicio14.py

--- Variables/ejercicio14.py
+++ Variables/ejercicio14.py
@@ -1,6 +1,6 @@
 #Solicitar al usuario una distancia en metros y transformar a km., cm. o mm. 
 metros = float(input("Ingrese la distancia en metros: "))
-distancia_km = metros / 1000
+distancia_km = metros // 1000
 distancia_cm = metros * 100
 distancia_mm = metros * 1000
 print("La distancia en Km es:", distancia_km)
